a44f185c3e4fd84b187e6ebe0e68ccc7ca4ad678
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-03 21:32:22 -0700

    udpate navbar to reference auth.profile instead of $scope.user

--- public/js/navbar/navbarController.js
+++ public/js/navbar/navbarController.js
@@ -2,21 +2,7 @@ angular.module('app.navbar', ['app.services'])
 
 .controller('NavbarController', function ($scope, $state, auth, store) {
   var profile = store.get("profile");
-
-  console.log('auth in navbar controller: ', auth)
-
-  $scope.$watch(function() {
-    return store.get("profile");
-  }, function() {
-    var profile = store.get("profile");
-    if (!auth.profile) {
-      $scope.user = undefined;
-    } else {
-      $scope.user = auth.profile.nickname;
-    }
-    $scope.isLoggedIn = !!profile;
-    console.log('scope.user', $scope.user)
-  })
+  $scope.auth = auth;
 
   $scope.isLoggedIn = !!profile;
   
@@ -29,5 +15,6 @@ angular.module('app.navbar', ['app.services'])
     store.remove('profile');
     store.remove('token');
     $scope.isLoggedIn = false;
+    $state.go('landing');
   }
 });
